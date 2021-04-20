c53e56dd645ff48a467c40b3c7ebb5a41c910421
MugiwaraDylan <352995@student.fontys.nl>
2021-04-20 15:18:32 +0200

    added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render( <
-    h1 > Hello world! < /h1>,
+import MyFirstComponent from './MyFirstComponent';
+
+ReactDOM.render(
+
+    <
+    MyFirstComponent > < /MyFirstComponent>,
     document.getElementById('root')
 );
